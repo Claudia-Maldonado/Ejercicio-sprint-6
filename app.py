4d86a6c4e40706966cf464135966319e6765f93c
Claudia Maldonado <claudiamaldonadoavalos@gmail.com>
2024-09-02 10:20:05 -0400

    Agregando el segundo gráfico

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st 
 
-car_data = pd.read_csv('vehicles_us.csv')
+car_data = pd.read_csv('C:/Users/USUARIO/Maldonado/Trabajos DA/Ejercicio-sprint-6/vehicles_us.csv')
+
 print (car_data)
-st.header('Venta de Vehículos')
+st.header('Venta de Vehículos por kilometraje')
 hist_button = st.button('Construir Histograma')
 scatter_plot_button = st.button('Construir Gráfico de Dispersión')
 
@@ -14,4 +15,10 @@ if hist_button:
     fig = px.histogram(car_data, x="odometer")
     
     st.plotly_chart(fig, use_container_width=True)
+    
+if scatter_plot_button:
+    st.write('Creación de gráfico de dispersión para el conjunto de datos.')
+    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig_scatter, use_container_width=True)
+
     
